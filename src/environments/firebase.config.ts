51d9eeec282d7fb52cfbe141262ab74d84913945
Greg Peters <gregorypeteryanoc.gpy@gmail.com>
2026-01-27 03:09:16 +0800

    apply security for envs

--- src/environments/firebase.config.ts
+++ src/environments/firebase.config.ts
@@ -1,10 +1,10 @@
 // Firebase configuration
 // Replace these values with your Firebase project configuration
 export const firebaseConfig = {
-  projectId: 'public-local-db',
-  appId: '1:832042779226:web:153647506c353b985a0e63',
-  storageBucket: 'public-local-db.firebasestorage.app',
-  apiKey: 'AIzaSyDXOzgKmpzuwrGd188upIZQIPAjqLCCDZo',
-  authDomain: 'public-local-db.firebaseapp.com',
-  messagingSenderId: '832042779226',
+  projectId: import.meta.env['NG_APP_FIREBASE_PROJECT_ID'],
+  appId: import.meta.env['NG_APP_FIREBASE_APP_ID'],
+  storageBucket: import.meta.env['NG_APP_FIREBASE_STORAGE_BUCKET'],
+  apiKey: import.meta.env['NG_APP_FIREBASE_API_KEY'],
+  authDomain: import.meta.env['NG_APP_FIREBASE_AUTH_DOMAIN'],
+  messagingSenderId: import.meta.env['NG_APP_FIREBASE_MESSAGING_SENDER_ID'],
 };
